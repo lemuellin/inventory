00c21d6c8d1dc11cd9966288fe50776caefa07e7
lemuellin <lemuellin@gmail.com>
2023-02-28 21:14:11 -0800

    add middleware - helmet, compression

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog');
 
+const compression = require("compression");
+const helmet = require("helmet");
+
 var app = express();
+
+app.use(helmet());
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
@@ -28,6 +33,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
